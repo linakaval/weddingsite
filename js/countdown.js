daa7da442248a8de9decc44460b541376bb2b7f6
Lina Kaval <kavalla@mail.uc.edu>
2024-04-29 19:41:35 -0400

    added countdown to home page, tentatively set to 4/30/25

---
+++ js/countdown.js
@@ -0,0 +1,27 @@
+// Set the date for the countdown (in this example, 1 day from now)
+var countDownDate = new Date(2025, 4, 30);
+
+// Update the countdown every second
+var x = setInterval(function() {
+    // Get the current date and time
+    var now = new Date().getTime();
+    
+    // Calculate the remaining time
+    var distance = countDownDate - now;
+    
+    // Calculate days, hours, minutes, and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Display the countdown in the element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the countdown is over, display a message
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000); // Update every second
